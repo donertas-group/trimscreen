f3af563eb2178ee00163b6ef9c911582ef319061
Yi Wang <wangyi9122@yahoo.com>
2025-09-29 11:30:06 +0200

    added a more detailed note on dada2 taxonomy

--- bin/taxref_reformat_standard.sh
+++ bin/taxref_reformat_standard.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # Uses preformatted databases from DADA2 (https://benjjneb.github.io/dada2/training.html)
+# Note that The DADA2 authors maintain and recommend pre-formatted training sets and species assignment files (hosted on https://benjjneb.github.io/dada2/training.html)
+# SILVA 138.2 uses nr99 taxonomy
 # The file for taxonomy assignment, identified by containing "train" in the name
 gunzip -c *train*gz > assignTaxonomy.fna
 
